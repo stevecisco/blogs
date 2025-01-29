469a35a2cdab9353e0ebac1cd1b889fd802b9084
Stephen Cisco <steve.cisco@centricconsulting.com>
2025-01-29 11:41:37 -0500

    Initial commit, fresh MAUI project, no provisioning yet

---
+++ Demo1/stevejcisco.maui.demo1/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace stevejcisco.maui.demo1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
